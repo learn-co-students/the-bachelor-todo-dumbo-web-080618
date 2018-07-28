eb105ab28bf0e4b3410a8038de102324e25c7154
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-28 13:34:05 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,25 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
-end
+require 'pry'
 
-def get_contestant_name(data, occupation)
-  # code here
+def get_first_name_of_season_winner(data, season)
+  data.each do |season_num, details|
+    if season_num.to_s == season
+      details.each do |info|
+        info.each do |key,value|
+          if key == :status
+            if value == "Winner"
+              return info[:name].split(" ").first
+            end
+          end 
+        end
+      end
+    end
+  end
 end
+  
+
+#get_contestant_name(data,"Chiropractic Assistant")
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
 end
 
 def get_occupation(data, hometown)
