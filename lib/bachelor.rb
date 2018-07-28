0d79c31de67c2fb0a76a35883a59a67177a5ddac
Juan Elias Castillo <eliasiscoding@gmail.com>
2018-07-28 22:10:00 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,30 +2,80 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   data.each do |season_num, details|
-    if season_num.to_s == season
+    if season_num == season
       details.each do |info|
         info.each do |key,value|
-          if key == :status
+          if key == "status"
             if value == "Winner"
-              return info[:name].split(" ").first
-            end
+              return info["name"].split(" ").first
+            end 
           end 
         end
       end
     end
   end
 end
-  
 
-#get_contestant_name(data,"Chiropractic Assistant")
+def get_contestant_name(data, occupation)
+ data.each do |season_num, details|
+      details.each do |info|
+        info.each do |key,value|
+          if key == "occupation"
+            if value == occupation
+              return info["name"]
+            end
+          end
+        end
+      end
+    end
+end
 
 def count_contestants_by_hometown(data, hometown)
+  counter = 0
+    data.each do |season_num, details|
+      details.each do |info|
+        info.each do |key,value|
+          if key == "hometown"
+            if value == hometown 
+              counter += 1 
+          end
+        end
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
-end
+    occupations = []
+    data.each do |season_num, details|
+      details.each do |info|
+        info.each do |key,value|
+          if key == "hometown"
+            if value == hometown
+              occupations.push (info["occupation"])
+            end
+          end
+        end
+      end
+    end
+    occupations.first
+  end
 
 def get_average_age_for_season(data, season)
-  # code here
+  add = []
+   data.each do |season_num, details|
+    if season_num == season
+      details.each do |info|
+        info.each do |key,value|
+          if key == "age"
+            add << value.to_i
+          end
+        end
+      end
+    end
+  end
+   average = add.inject(1) { |num,el| num + el}.to_f / add.size
+   return average.round
 end
+
